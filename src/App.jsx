cb02a6f73154347a718255ca5c9a835fa3ef7f36
Your Name <you@example.com>
2024-11-18 23:26:40 +0545

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import React Router components
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import About from './Components/About/About';
-import Services from './Components/Services/Services';
-import MyWork from './Components/MyWork/MyWork';
-import Contact from './Components/Contact/Contact';
-import Footer from './Components/Footer/Footer';
-import './index.css';  // Assuming this file contains Tailwind directives
-import ScrollToTop from './Components/ScrollToTop ';
-import Project from './Components/MyWork/Project';
-import ContactPage from './Components/Contact/ContactPage';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import React Router components
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
+import Services from "./Components/Services/Services";
+import MyWork from "./Components/MyWork/MyWork";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import "./index.css"; // Assuming this file contains Tailwind directives
+import ScrollToTop from "./Components/ScrollToTop ";
+import Project from "./Components/MyWork/Project";
+import ContactPage from "./Components/Contact/ContactPage";
+import ScrollToTopPage from "./Components/ScrollToTopPage";
 
 const App = () => {
   return (
-    <Router basename="/portfolio" >
+    <Router basename="/portfolio">
+      <ScrollToTopPage />
       <Routes>
         {/* Route for the main homepage */}
         <Route
@@ -28,12 +30,12 @@ const App = () => {
               <MyWork />
               <Contact />
               <Footer />
-              <ScrollToTop/>
+              <ScrollToTop />
             </>
           }
         />
-        <Route path='/project' element={<Project/>}/>
-        <Route path='/contact' element={<ContactPage/>}/>
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
